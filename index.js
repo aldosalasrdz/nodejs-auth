cad8b0821cfac19901a732d488502ed864da6fef
Aldo Salas <aldosalasrdz@gmail.com>
2022-03-07 21:36:44 -0600

    Routing con Express.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('My App')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/home', (req, res) => {
+  res.send('Bienvenido al sitio web de PROFLOR DEL NORTE')
+})
+
+app.get('/categories', (req, res) => {
+  res.send('Descubre las categorias de nuestra tienda')
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  })
+})
+
+app.listen(port, () => {
+  console.log('Mi port', port)
+})
