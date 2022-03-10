69761418e034a62c295498b5fdd41661825f4350
Aldo Salas <aldosalasrdz@gmail.com>
2022-03-09 22:46:12 -0600

    POST: método para crear

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const routerApi = require('./routes')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/home', (req, res) => {
   res.send('Bienvenido al sitio web de PROFLOR DEL NORTE')
 })
