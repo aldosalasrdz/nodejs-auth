7db12258f8720dd84b46eab29e8feb51bf081edd
Aldo Salas <aldosalasrdz@gmail.com>
2022-03-11 23:19:35 -0600

    PUT, PATCH y DELETE

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const faker = require('faker')
+
 const router = express.Router()
 
 
 router.get('/', (req, res) => {
-  res.send('Descubre las categorias de nuestra tienda')
+  const categories = []
+  const { size } = req.query
+  const limit = size || 10
+  for (let i = 0; i < limit; i++) {
+    categories.push({
+      category: faker.commerce.department(),
+    })
+  }
+  res.json(categories)
 })
 
 // Endpoint con múltiples parámetros
@@ -19,7 +29,33 @@ router.get('/:id', (req, res) => {
   const { id } = req.params
   res.json({
     id,
-    category: 'Product category'
+    category: faker.commerce.department()
+  })
+})
+
+router.post('/', (req, res) => {
+  const body = req.body
+  res.json({
+    message: 'created',
+    data: body
+  })
+})
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params
+  const body = req.body
+  res.json({
+    message: 'update',
+    data: body,
+    id
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+  res.json({
+    message: 'deleted',
+    id
   })
 })
 
--- routes/users.router.js
+++ routes/users.router.js
@@ -13,4 +13,40 @@ router.get('/', (req, res) => {
   }
 })
 
+router.post('/', (req, res) => {
+  const { limit, offset } = req.query
+  if (limit && offset) {
+    res.json({
+      message: 'created',
+      data: {
+        limit,
+        offset
+      }
+    })
+  } else {
+    res.json({
+      message: 'No hay parametros'
+    })
+  }
+})
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params
+  const { limit, offset } = req.query
+  if (limit && offset) {
+    res.json({
+      message: 'update',
+      data: {
+        limit,
+        offset
+      },
+      id
+    })
+  } else {
+    res.json({
+      message: 'No hay parametros'
+    })
+  }
+})
+
 module.exports = router
