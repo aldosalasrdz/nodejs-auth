818dab5a4e765746ba1453659dcb206eaba6d28f
Aldo Salas <aldosalasrdz@gmail.com>
2022-04-14 04:50:14 -0500

    Añadiendo modelos de producto, categoria y usuario

---
+++ db/models/index.js
@@ -0,0 +1,11 @@
+const { User, UserSchema } = require('./user.model')
+const { Category, CategorySchema } = require('./category.model')
+const { Product, ProductSchema } = require('./product.model')
+
+function setupModels(sequelize) {
+  User.init(UserSchema, User.config(sequelize))
+  Category.init(CategorySchema, Category.config(sequelize))
+  Product.init(ProductSchema, Product.config(sequelize))
+}
+
+module.exports = setupModels
