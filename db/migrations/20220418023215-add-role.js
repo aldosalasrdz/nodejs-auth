3ae951dc55efd414bf205aee777d772684b845bb
Aldo Salas <aldosalasrdz@gmail.com>
2022-04-24 20:59:33 -0500

    Quitando punto y coma en el codigo

--- db/migrations/20220418023215-add-role.js
+++ db/migrations/20220418023215-add-role.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const { UserSchema, USER_TABLE } = require('../models/user.model')
 
--- db/migrations/20220419054455-create-customers.js
+++ db/migrations/20220419054455-create-customers.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const { CustomerSchema, CUSTOMER_TABLE } = require('../models/customer.model')
 
