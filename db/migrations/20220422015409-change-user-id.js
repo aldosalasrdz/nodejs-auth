500ca0523eca2ef22cbbe106ad8b998e35e38aea
Aldo Salas <aldosalasrdz@gmail.com>
2022-04-26 04:01:52 -0500

    Resolviendo las relaciones uno a uno

---
+++ db/migrations/20220422015409-change-user-id.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const { DataTypes } = require('sequelize')
+
+const { CUSTOMER_TABLE } = require('../models/customer.model')
+
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.changeColumn(CUSTOMER_TABLE, 'user_id', {
+      allowNull: false,
+      field: 'user_id',
+      type: DataTypes.INTEGER,
+      unique: true
+    })
+  },
+
+  async down (queryInterface) {
+    // await queryInterface.removeColumn(CUSTOMER_TABLE)
+  }
+}
--- db/models/index.js
+++ db/models/index.js
@@ -10,6 +10,7 @@ function setupModels(sequelize) {
   Customer.init(CustomerSchema, Customer.config(sequelize))
 
   // Associations
+  User.associate(sequelize.models)
   Customer.associate(sequelize.models)
 }
 
