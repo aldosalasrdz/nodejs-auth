5134b240c2f533c52ee748ec9601df2b9623d4f2
Aldo Salas <aldosalasrdz@gmail.com>
2022-05-11 05:30:52 -0500

    Paginación

--- routes/products.router.js
+++ routes/products.router.js
@@ -2,19 +2,22 @@ const express = require('express')
 
 const ProductService = require('../services/product.service')
 const validatorHandler = require('../middlewares/validator.handler')
-const { createProductSchema, updateProductSchema, getProductSchema } = require('../schemas/product.schema')
+const { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema } = require('../schemas/product.schema')
 
 const router = express.Router()
 const service = new ProductService()
 
-router.get('/', async (req, res, next) => {
-  try {
-    const products = await service.findProducts()
-    res.json(products)
-  } catch (error) {
-    next(error)
+router.get('/',
+  validatorHandler(queryProductSchema, 'query'),
+  async (req, res, next) => {
+    try {
+      const products = await service.findProducts(req.query)
+      res.json(products)
+    } catch (error) {
+      next(error)
+    }
   }
-})
+)
 
 router.get('/:id',
   validatorHandler(getProductSchema, 'params'),
