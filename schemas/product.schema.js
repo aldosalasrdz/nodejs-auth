f715068da17a0d483dc382d1eb06aa8180eb1a9b
Aldo Salas <aldosalasrdz@gmail.com>
2022-05-12 03:36:57 -0500

    Filtrando precios con operadores

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -1,11 +1,15 @@
+/* eslint-disable camelcase */
 const Joi = require('joi')
 
 const id = Joi.number().integer()
 const name = Joi.string().min(3).max(255)
-const price = Joi.number().strict().integer().min(10)
+const price = Joi.number().integer().min(10)
 const description = Joi.string().min(10)
 const image = Joi.string().uri()
 const categoryId = Joi.number().integer()
+
+const min_price = Joi.number().integer()
+const max_price = Joi.number().integer()
 
 const limit = Joi.number().integer().min(1)
 const offset = Joi.number().integer().min(0)
@@ -32,7 +36,11 @@ const getProductSchema = Joi.object({
 
 const queryProductSchema = Joi.object({
   limit,
-  offset
-})
+  offset,
+  price,
+  min_price,
+  max_price: max_price.greater(Joi.ref('min_price'))
+}).with('min_price', 'max_price')
+  .with('max_price', 'min_price')
 
 module.exports = { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema }
--- services/product.service.js
+++ services/product.service.js
@@ -1,5 +1,7 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-useless-constructor */
 const boom = require('@hapi/boom')
+const { Op } = require('sequelize')
 
 const { models } = require('./../libs/sequelize')
 
@@ -13,13 +15,28 @@ class ProductService {
 
   async findProducts (query) {
     const options = {
-      include: ['category']
+      include: ['category'],
+      where: {}
     }
+
     const { limit, offset } = query
     if (limit && offset) {
       options.limit = limit
       options.offset = offset
     }
+
+    const { price } = query
+    if (price) {
+      options.where.price = price
+    }
+
+    const { min_price, max_price } = query
+    if (min_price && max_price) {
+      options.where.price = {
+        [Op.between]: [min_price, max_price]
+      }
+    }
+
     const products = await models.Product.findAll(options)
     return products
   }
